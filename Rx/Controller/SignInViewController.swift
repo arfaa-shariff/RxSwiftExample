0431ba518d20e0576e6171b2bdd54a7de9bca6ce
Arfaa Nisar Shariff <arfaa@plank.life>
2017-10-13 15:26:54 +0530

    Added a sign in view controller with rx functionality .

---
+++ Rx/Controller/SignInViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SignInViewController.swift
+//  Rx
+//
+//  Created by Arfaa Nisar Shariff on 13/10/17.
+//  Copyright © 2017 Arfaa Nisar Shariff. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import RxSwift
+import RxCocoa
+
+class SignInViewController: UIViewController{
+    
+    let disposeBag = DisposeBag()
+    @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var usernameTextfield: UITextField!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    override func viewDidLoad() {
+        setUpRx()
+    }
+    
+    func setUpRx(){
+        let userNameValid = usernameTextfield.rx.text.map{
+            ($0?.characters.count)! >= 5
+        }
+        
+        let passwordValid = passwordTextfield.rx.text.map({
+            ($0?.characters.count)! >= 5
+        })
+        
+        let everythingValid = Observable.combineLatest(userNameValid, passwordValid){$0 && $1}
+        
+        userNameValid.bind(to: passwordTextfield.rx.isEnabled).addDisposableTo(disposeBag)
+        
+        everythingValid.bind(to: signInButton.rx.isEnabled).addDisposableTo(disposeBag)
+        
+        
+        
+        signInButton.rx.tap.subscribe{event in
+            switch event{
+            case .next(let value):
+                print("On next method", value)
+            case .error(let error):
+                print(error)
+                
+            case .completed:
+                print("completed")
+                
+            }
+            
+        }
+    }
+}
